0e2a8bac522cb9dad7196fa91066ab57576867c6
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2022-05-29 22:21:02 +0300

    transactionHistory

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import user from 'components/Profile/user.json';
 import data from 'components/statistics/data.json';
 import friends from 'components/FriendList/friends.json';
+import transactions from 'components/TransactionHistory/transactions.json';
 
 import Profile from 'components/Profile/profile';
 import Statistics from 'components/statistics/statistics';
 import FriendList from 'components/FriendList/FriendList';
+import TransactionHistory from 'components/TransactionHistory/transactionHistory';
 
 export default function App() {
   return (
@@ -19,6 +21,7 @@ export default function App() {
       <Statistics title="Upload Stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
