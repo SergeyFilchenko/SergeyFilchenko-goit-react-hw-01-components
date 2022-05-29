74b0223c72e1905fff4401ee0c189978003fc66e
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2022-05-29 12:35:22 +0300

    statistics+

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import user from './components/profile/user.json';
+import data from './components/statistics/data.json';
 import Profile from './components/profile/profile';
+import Statistics from 'components/statistics/statistics';
 
-export default function App (){
-return(
-  <div>
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
-  </div>
-);
+export default function App() {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload Stats" stats={data} />
+      <Statistics stats={data} />
+    </div>
+  );
 }
