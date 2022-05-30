37e40ce7112b6a49f7503eb07da8d74c01ca3822
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2022-05-30 21:54:51 +0300

    ./

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import friends from 'data/friends.json';
 import transactions from 'data/transactions.json';
 
 import Profile from 'components/Profile/profile';
-import Statistics from 'components/Statistic/statistics';
+import Statistics from './components/Statistic/statistics';
 import FriendList from 'components/FriendList/friendList';
 import TransactionHistory from 'components/TransactionHistory/transactionHistory';
 
