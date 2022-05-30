778007db396a9cc3415a0a89ef408372b98d4431
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2022-05-30 23:07:53 +0300

    12

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import user from 'data/user.json';
-import data from 'data/data.json';
-import friends from 'data/friends.json';
-import transactions from 'data/transactions.json';
+import user from './data/user.json';
+import data from './data/statistics.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
-import Profile from 'components/Profile/Profile';
-import Statistics from 'components/Statistic/Statistics';
-import FriendList from 'components/FriendList/FriendList';
-import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistic/Statistics';
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
 export default function App() {
   return (
